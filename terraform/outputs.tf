fb4f936e49eb1e44436405fe01e2958afccc1f6e
zeaforx <ajibcaleb@gmail.com>
2026-02-09 10:19:25 +0100

    Untracked files

--- terraform/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "comfy_ui_url" {
-  description = "The URL to access ComfyUI"
-  value       = "http://${google_compute_instance.comfy-ui-server.network_interface.0.access_config.0.nat_ip}:8188"
-}
